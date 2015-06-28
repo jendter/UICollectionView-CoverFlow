7c0b2d7d78a8318e0bba08039ccc328ff5420b86
Josh Endter <jendter@users.noreply.github.com>
2015-06-27 21:11:02 -0400

    Changed to old school cover flow style

--- CoverFlow/CoverFlowLayout.m
+++ CoverFlow/CoverFlowLayout.m
@@ -16,36 +16,6 @@
 
 @implementation CoverFlowLayout
 
-//- (void)prepareLayout {
-//    
-//    NSLog(@"Prepare layout");
-//    
-//    self.cellAttributes = [NSMutableArray array];
-//    
-//    CGSize contentSize = [self collectionViewContentSize];
-//    
-//    NSUInteger cellCount = [self.collectionView numberOfItemsInSection:0];
-//    
-//    
-//    
-//    for (int i = 0; i < cellCount; i++) {
-//        
-//        CGFloat randX = arc4random_uniform(contentSize.width);
-//        CGFloat randY = arc4random_uniform(contentSize.height);
-//        
-//        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:i inSection:0];
-//        
-//        UICollectionViewLayoutAttributes *attr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
-//        
-//        attr.center = (CGPoint){randX, randY};
-//        
-//        attr.size = (CGSize){100,100};
-//        
-//        [self.cellAttributes addObject:attr];
-//    }
-//
-//    
-//}
 
 - (void)prepareLayout {
 
@@ -59,32 +29,60 @@
     visibleRegion.origin = self.collectionView.contentOffset;
     visibleRegion.size   = self.collectionView.bounds.size;
     
-    // Modify the layout attributes as needed here
-    
-    //UICollectionView *collectionView = [self collectionView];
-    
-    
     for (UICollectionViewLayoutAttributes *attributesForSingleElement in attributesForAllRectElements) {
         //NSLog(@"attributes: %@", attributesForSingleElement);
         //NSLog(@"Center: {%f,%f}", attributesForSingleElement.center.x, attributesForSingleElement.center.y);
        
         CGFloat distanceFromCenterInPoints = fabs(attributesForSingleElement.center.x-(visibleRegion.origin.x+(visibleRegion.size.width/2)));
-        CGFloat distanceFromCenterInRangeZeroToOne = (visibleRegion.size.width - distanceFromCenterInPoints)/visibleRegion.size.width;
+        CGFloat distanceFromCenterInPointsWithNegativeNums = attributesForSingleElement.center.x-(visibleRegion.origin.x+(visibleRegion.size.width/2));
         
-        NSLog(@"The element at index: {%ld-%ld} is %f points from the center of the visable region.", attributesForSingleElement.indexPath.section, attributesForSingleElement.indexPath.item, distanceFromCenterInPoints );
+        CGFloat distanceFromCenterInRangeZeroToOne = (visibleRegion.size.width - distanceFromCenterInPoints)/visibleRegion.size.width;
+        CGFloat distanceFromCenterInRangeZeroToOneWithNegativeNums = (visibleRegion.size.width - distanceFromCenterInPointsWithNegativeNums)/visibleRegion.size.width;
+        
+        
+        NSLog(@"The element at index: {%ld-%ld} is %f points from the center of the visible region.", attributesForSingleElement.indexPath.section, attributesForSingleElement.indexPath.item, distanceFromCenterInPoints );
         
         
         attributesForSingleElement.alpha =  distanceFromCenterInRangeZeroToOne;
-        //attributesForSingleElement.transform = CGAffineTransformMakeRotation(0.2);
         
         CGFloat itemScaleFactor = 1;
-        //if (distanceFromCenterInRangeZeroToOne > 0.8) {
-            itemScaleFactor = itemScaleFactor + distanceFromCenterInRangeZeroToOne*0.45;
-        //}
+        itemScaleFactor = itemScaleFactor + distanceFromCenterInRangeZeroToOne*0.45; // The center item should get bigger
         
-        attributesForSingleElement.zIndex = distanceFromCenterInRangeZeroToOne*100;
+        attributesForSingleElement.zIndex = distanceFromCenterInRangeZeroToOne*100; // So that the center element is always on top
         
         attributesForSingleElement.transform3D = CATransform3DScale(attributesForSingleElement.transform3D, itemScaleFactor, itemScaleFactor, 0);
+        
+//        CGFloat rotationConstant;
+//        if (distanceFromCenterInPointsWithNegativeNums < 0) {
+//            rotationConstant = distanceFromCenterInRangeZeroToOne*-1;
+//        } else {
+//            rotationConstant = distanceFromCenterInRangeZeroToOne;
+//        }
+        
+        CGFloat degreesToRotate = (distanceFromCenterInRangeZeroToOneWithNegativeNums-1) * 230;
+        
+        if (degreesToRotate < -60.0) {
+            degreesToRotate = -60.0;
+        } else if (degreesToRotate > 60.0) {
+            degreesToRotate = 60.0;
+        }
+        
+        //degreesToRotate = fabs(degreesToRotate);
+        
+        NSLog(@"Degrees to rotate: %f", degreesToRotate);
+        
+        // Rotate for
+        //attributesForSingleElement.transform3D = CATransform3DRotate(attributesForSingleElement.transform3D, DegreesToRadians(degreesToRotate), 0, 1, 0);
+        
+        CATransform3D t = attributesForSingleElement.transform3D;
+        //Add the perspective!!!
+        t.m34 = 1.0/ -500;
+        t = CATransform3DRotate(t, degreesToRotate * M_PI / 180.0f, 0, 1, 0);
+        attributesForSingleElement.transform3D = t;
+        
+        
+        // Rotate everything for a slight top down perspective
+        //attributesForSingleElement.transform3D = CATransform3DRotate(attributesForSingleElement.transform3D, DegreesToRadians(20), 0.1, 0, 0);
     }
     
     return attributesForAllRectElements;
@@ -93,5 +91,9 @@
 -(BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
     return YES;
 }
+
+CGFloat DegreesToRadians(CGFloat degrees) {
+    return degrees * M_PI / 180;
+};
 
 @end
--- CoverFlow/CoverFlowViewController.m
+++ CoverFlow/CoverFlowViewController.m
@@ -18,8 +18,6 @@
 
 @implementation CoverFlowViewController
 
-//static NSString * const reuseIdentifier = @"CoverFlowCell";
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -38,16 +36,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark <UICollectionViewDataSource>
 
@@ -141,15 +129,6 @@
     
     NSLog(@"%@", self.photos);
 }
-
-
-
-
-
-
-
-
-
 
 
 #pragma mark <UICollectionViewDelegate>
